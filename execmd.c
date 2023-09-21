588cb1c2344bba5635442dc19f2860898c2dc8cd
jonnes-chikwezga <joneschikwezga4@gmail.com>
2023-09-21 13:10:46 -0700

    simple_shell

--- execmd.c
+++ execmd.c
@@ -1,22 +1,30 @@
 #include "main.h"
-/**
- * execmd - Entry Point
- * @argv: command arguments typed by user
- *
- */
-void execmd(char **argv)
-{
-char *command = NULL, *actual_command = NULL;
 
-if (argv)
+/**
+ * execute_command - Execute a shell command.
+ *
+ * This function takes an array of command arguments and attempts to execute
+ * the specified command.
+ *
+ * @param command_args An array of strings representing the command and its arguments.
+ *                    The first element (command_args[0]) should be the command name,
+ *                    followed by its arguments. The array must be NULL-terminated.
+ */
+void execute_command(char **command_args)
 {
-command = argv[0];
+char *command = NULL;
+char *actual_command = NULL;
+
+if (command_args)
+{
+command = command_args[0];
 
 actual_command = get_location(command);
 
-if (execve(actual_command, argv, NULL) == -1)
+if (execve(actual_command, command_args, NULL) == -1)
 {
-perror("Error:");
+perror("shell: execution error");
 }
 }
 }
+
